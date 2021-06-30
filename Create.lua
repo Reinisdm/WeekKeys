9062c86758226ab7c8bb3d8dd11d40e011482f34
Reinisdm <65504797+Reinisdm@users.noreply.github.com>
2021-06-30 07:53:23 +0300

    fix texture coords

--- Create.lua
+++ Create.lua
@@ -1,5 +1,5 @@
 local Addon = ...
-local VERSION = 111
+local VERSION = 112
 local buttons = {}
 local elements = {}
 local buttonID = 1
--- UI.lua
+++ UI.lua
@@ -397,11 +397,11 @@ function WeekKeys.UI.LootFinderButton(name,parent)
         elseif source == "raid" then
             self.source:SetTexture('interface/minimap/objecticonsatlas.blp')
             --                      0.5009765625,0.5224609375,0.294921875,0.337890625
-            self.source:SetTexCoord(0.283203125, 0.3046875, 0.94140625, 0.984375)
+            self.source:SetTexCoord(0.5009765625,0.5224609375,0.294921875,0.337890625)
         elseif source == "instance" then--34060
             self.source:SetTexture('interface/minimap/objecticonsatlas.blp')
             --                      0.1728515625,0.1943359375,0.912109375,0.955078125
-            self.source:SetTexCoord(0.24609375,0.267578125,0.951171875,0.994140625)
+            self.source:SetTexCoord(0.1728515625,0.1943359375,0.912109375,0.955078125)
         else
             self.source:SetTexture()
         end
